9d4b9564d57e710a566227b63197fd3e46428943
Your Name <you@example.com>
2024-11-19 12:55:16 +0900

    감지 데이터 베이스 모델 추가

--- server/routers/detect.py
+++ server/routers/detect.py
@@ -9,14 +9,20 @@ import torch
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 router = APIRouter(prefix="/api")
 
+class detectData(BaseModel):
+    file : UploadFile = File(...)
+    type : str
+    fileFath : str
+
+
 @router.post("/detect_frame/")
-async def upload_frame(file: UploadFile = File(...)):
+async def upload_frame(data:detectData):
     """
     클라이언트에서 업로드한 프레임에서 객체 탐지를 수행합니다.
     """
     try:
         # 파일 내용 읽기
-        contents = await file.read()
+        contents = await data.file.read()
 
         # Numpy 배열로 변환
         np_arr = np.frombuffer(contents, np.uint8)
@@ -28,9 +34,10 @@ async def upload_frame(file: UploadFile = File(...)):
         # 탐지 결과 처리
         detections = results.pandas().xyxy[0].to_dict(orient="records")  # 탐지 결과를 딕셔너리로 변환
 
-        # 탐지 결과 이미지 생성 (시각화 용도)
-        detection_img = np.squeeze(results.render())  # 탐지 결과가 그려진 이미지
-        #cv2.imwrite("detected_frame.jpg", detection_img)  # 테스트용으로 이미지 저장
+        if data.type == 'image':
+            # 탐지 결과 이미지 생성 (시각화 용도)
+            detection_img = np.squeeze(results.render())  # 탐지 결과가 그려진 이미지
+            #cv2.imwrite("detected_frame.jpg", detection_img)  # 테스트용으로 이미지 저장
 
         return JSONResponse({"detections": detections})
 
